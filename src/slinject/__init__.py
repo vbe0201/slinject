334de36341b8c0eb020116022806c70662342184
Valentin B <valentin.be@protonmail.com>
2020-04-29 22:13:42 +0200

    Initialize project

---
+++ src/slinject/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+slinject
+~~~~~~~~
+
+The DLL Injection Toolkit for Python.
+
+:copyright: (c) 2020 Valentin B.
+:license: MIT, see LICENSE for further details.
+"""
+
+from .meta import *
---
+++ src/slinject/meta.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+__all__ = (
+    '__author__',
+    '__copyright__',
+    '__license__',
+    '__title__',
+    '__version__',
+)
+
+__author__ = 'Valentin B.'
+__copyright__ = 'Copyright 2020 Valentin B.'
+__license__ = 'MIT'
+__title__ = 'slinject'
+__version__ = '0.1.0'
